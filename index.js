c387e5fcfb191c4642bc3fc5ef3b6029d53e35af
ChristineV67 <christinevoinson@yahoo.com>
2024-10-15 12:40:46 +0200

    wilders

---
+++ index.js
@@ -0,0 +1,12 @@
+import chalk from "chalk";
+
+console.log(chalk.blue('Gabi'));
+console.log(chalk.green('Victor'));
+console.log(chalk.yellow('Romain'));
+console.log(chalk.red('Mathieu'));
+
+
+
+
+
+
